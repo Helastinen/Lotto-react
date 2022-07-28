645ab25768532772d5033c6c8cfb7382a81485bb
Antti <hellsten.antti@gmail.com>
2022-07-28 17:56:27 +0300

    refactor themes

---
+++ src/themes.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+// eslint-disable-next-line no-unused-vars
+export const drawnNumberTheme = createTheme({
+	palette: {
+		warning: {
+			main: "#ffa500",
+		}
+	}
+});
+
+
+
